64e32105c0339153e53801e857cdb23aae2ae630
HanUL072 <dbstjrals125@naver.com>
2025-08-19 14:06:17 +0900

    learn. 함수형 컴포넌트의 상태 관리 (초기화 함수)

--- 10-stateful-component/src/components/update-input/index.jsx
+++ 10-stateful-component/src/components/update-input/index.jsx
@@ -5,22 +5,14 @@ import './style.css'
  * UpdateInput 컴포넌트
  * @param {Object} props
  * @param {string} props.label - 인풋 요소의 레이블
+ * @param {string} props.value - 인풋 요소의 초깃값
  */
-export default function UpdateInput({ label }) {
+export default function UpdateInput({ label, value }) {
   // console.log('UpdateInput 컴포넌트 렌더링')
 
-  // 리액트 훅 함수를 사용해 함수현 컴포넌트의 상태 관리
-  const [state, setState] = useState('') //[state, setState] -> [name, setName]
+  // 리액트 훅 함수를 사용해 함수형 컴포넌트의 상태 관리
+  const [state, setState] = useState(value)
   const id = useId()
-
-  // console.log(stateAndUpdater.at(0)) // state
-  // console.log(stateAndUpdater.at(1)) // setState()
-
-  // 상태 선언
-  // const name = '한울'
-
-  // 상태 업데이트
-  // const setName = () => {}
 
   // 이벤트 리스너
   const handleInput = (e) => {
@@ -34,7 +26,7 @@ export default function UpdateInput({ label }) {
         <label htmlFor={id}>{label}</label>
         <input id={id} type="text" defaultValue={state} onInput={handleInput} />
       </div>
-      <output>{state}</output>
+      <output>{state || '입력된 상태 값'}</output>
     </>
   )
 }
