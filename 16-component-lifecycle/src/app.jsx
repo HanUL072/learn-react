94c6ea7d502640dc252a1ffd582d47f3b5d0f694
HanUL072 <dbstjrals125@naver.com>
2025-08-25 11:44:11 +0900

    learn. 리액트가 못하는 것들 - 렌더링 프로세스와 무관 - 리액트의 렌더링 외적인 요인들 (이벤트 핸들링, 초점 이동 등)

--- 16-component-lifecycle/src/app.jsx
+++ 16-component-lifecycle/src/app.jsx
@@ -7,10 +7,6 @@ export default function App() {
   const [isVisible, setIsVisible] = useState(true)
   const handleInput = (e) => setIsVisible(e.target.checked)
 
-  const [inputValue, setInputValue] = useState('Child 컴포넌트')
-  const [headline, setHeadline] = useState('사자 보이즈')
-  const updateHeadline = () => setHeadline((h) => h + '🦁')
-
   return (
     <LearnSection
       className="p-10"
@@ -25,14 +21,7 @@ export default function App() {
         />
         Child 컴포넌트 표시 ({isVisible.toString()})
       </label>
-      {isVisible ? (
-        <Child
-          headline={headline}
-          updateHeadline={updateHeadline}
-          inputValue={inputValue}
-          setInputValue={setInputValue}
-        />
-      ) : null}
+      {isVisible ? <Child /> : null}
     </LearnSection>
   )
 }
@@ -41,20 +30,37 @@ export default function App() {
 // 1. 생성(mount)
 // 2. 변경(update) x N
 // 0. 소멸(unmount)
-function Child({ headline, updateHeadline, inputValue, setInputValue }) {
+function Child() {
   console.log('Child 렌더링')
+
+  // 일반 변수 정의
+  let count = 10
+
+  const [headline, setHeadline] = useState('Child 컴포넌트')
 
   return (
     <article className="mt-5 p-5 border-2 border-inherit">
-      <h2 className="text-xl font-extrabold mb-2">{headline}</h2>
-      <input
-        type="text"
-        className="input"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button type="button" className="button mt-2" onClick={updateHeadline}>
-        사자 이모지 추가
+      <h2>{headline}</h2>
+      <button
+        type="button"
+        className="button mt-2"
+        onClick={(e) => {
+          // 이벤트 핸들러 (사용자에 의해 브라우저에서 실행)
+          // 리액트 렌더링 프로세스와는 전혀 무관!!!!
+          //
+          // 명령형 프로그래밍
+          //
+          // 상태를 사용하지 않고 (가상 DOM을 사용하지 않고)
+          // 직접 DOM에 접근/조작
+          count = count + 10
+          console.log(`updated count value = ${count}`)
+          e.target.textContent = String(count)
+          // 초점 이동시키고자 한다면?
+          // 리액트가 못하는 일 (부수효과)
+          document.querySelector('.input').select()
+        }}
+      >
+        {count}
       </button>
     </article>
   )
