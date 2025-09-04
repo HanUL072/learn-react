9d3b0133c0d27a5ccabf9b3515e4de1aaace17a4
HanUL072 <dbstjrals125@naver.com>
2025-09-05 08:59:28 +0900

    add. sonner 라이브러리 추가 및 toast 설정

--- learn-react/31-form-validation/src/app.tsx
+++ learn-react/31-form-validation/src/app.tsx
@@ -2,8 +2,21 @@ import { type ComponentProps, type ReactNode, useId } from 'react'
 import { type FieldValues, type SubmitHandler, useForm } from 'react-hook-form'
 import { type ClassValue } from 'clsx'
 import { Eye, EyeOff, HelpCircle } from 'lucide-react'
+import { toast } from 'sonner'
 import { useToggleState } from './hooks'
 import { tw } from './utils'
+
+const validateEmail = (value: string) => {
+  if (!value.endsWith('@likelion.dev'))
+    return '메일 주소는 @likelion.dev로 끝나야 합니다.'
+}
+
+const validatePassword = (value: string) => {
+  if (!/[a-z]/.test(value)) return '영문 소문자를 하나 이상 포함해야 합니다.'
+  if (!/[A-Z]/.test(value)) return '영문 대문자를 하나 이상 포함해야 합니다.'
+  if (!/[0-9]/.test(value)) return '숫자를 하나 이상 포함해야 합니다.'
+  if (value.length < 8) return '8자리 이상 입력해야 합니다.'
+}
 
 export default function LoginForm() {
   const [showPassword, { toggle }] = useToggleState(false)
@@ -12,9 +25,19 @@ export default function LoginForm() {
     register,
     formState: { errors },
     handleSubmit,
+    reset,
   } = useForm()
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    toast.success(JSON.stringify(data, null, 2), {
+      action: {
+        label: 'Reset',
+        onClick: () => {
+          reset({ email: '', password: '' })
+        },
+      },
+    })
+
     console.log(data)
   }
 
@@ -30,10 +53,7 @@ export default function LoginForm() {
         error={errors.email?.message as string}
         inputProps={register('email', {
           required: { value: true, message: '이메일 입력은 필수입니다.' },
-          validate: (value: string) => {
-            if (!value.endsWith('@likelion.dev'))
-              return '메일 주소는 @likelion.dev로 끝나야 합니다.'
-          },
+          validate: validateEmail,
         })}
       />
       <PasswordInput
@@ -42,14 +62,7 @@ export default function LoginForm() {
         error={errors.password?.message as string}
         inputProps={register('password', {
           required: { value: true, message: '패스워드 입력은 필수입니다.' },
-          validate: (value: string) => {
-            if (!/[a-z]/.test(value))
-              return '영문 소문자를 하나 이상 포함해야 합니다.'
-            if (!/[A-Z]/.test(value))
-              return '영문 대문자를 하나 이상 포함해야 합니다.'
-            if (!/[0-9]/.test(value)) return '숫자를 하나 이상 포함해야 합니다.'
-            if (value.length < 8) return '8자리 이상 입력해야 합니다.'
-          },
+          validate: validatePassword,
         })}
       />
       <SubmitButton />
