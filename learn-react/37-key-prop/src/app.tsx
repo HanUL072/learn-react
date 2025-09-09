ebca188f70f16e9096ca6bafb220976fa7d43b77
HanUL072 <dbstjrals125@naver.com>
2025-09-09 14:53:24 +0900

    learn. 이벤트 전파 단계 - 캡처링 단계 - 타켓 단계 - 버블링 단계 - 포털에서도 이벤트 전파

--- learn-react/37-key-prop/src/app.tsx
+++ learn-react/37-key-prop/src/app.tsx
@@ -1,10 +1,26 @@
-import { LearnSection } from '@/components'
-import RandomCountUp from './demo'
+import { useCallback } from 'react'
+import { Child } from '@/components'
 
 export default function App() {
+  const makeHandleClick = useCallback(
+    (message: string) => () => console.log(message),
+    []
+  )
+
   return (
-    <LearnSection title="랜덤 카운트 업" className="p-10">
-      <RandomCountUp />
-    </LearnSection>
+    <div
+      role="presentation"
+      className="p-5 bg-indigo-600 text-white"
+      onClickCapture={makeHandleClick('부모 div - Capture 단계')}
+      onClick={makeHandleClick('부모 div - Bubble 단계')}
+    >
+      <h2 className="m-0">부모</h2>
+      <Child
+        onClickCapture={makeHandleClick('자식 div - Capture 단계')}
+        onClick={makeHandleClick('자식 div - Bubble 단계')}
+      >
+        자식
+      </Child>
+    </div>
   )
 }
