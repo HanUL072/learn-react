7df1cee0f4b156f8311d1bab7ad41cd579751819
HanUL072 <dbstjrals125@naver.com>
2025-09-22 16:49:21 +0900

    learn. 에러 UI - 글로벌 error.tsx 컴포넌트 추가 - 데이터 가져오는 중에 오류가 발생하면 대체 UI를 화면에 표시

--- learn-next-js/05-loading-error/src/app/books/api.ts
+++ learn-next-js/05-loading-error/src/app/books/api.ts
@@ -5,6 +5,7 @@
 
 // --------------------------------------------------------------------------
 
+import { wait } from '@/utils'
 import type { QuoteResponse } from './types'
 
 // 데이터 가져오기 비동기 함수
@@ -13,21 +14,8 @@ export const fetchQuotes = async (): Promise<QuoteResponse['quotes']> => {
   const response = await fetch('https://dummyjson.com/quotes')
   const { quotes } = await response.json()
 
-  return quotes // 리액트의 use 함수 활용
-  // const { quotes } = use<QuoteResponse>(
-  //   fetch('https://dummyjson.com/quotes').then((response) => response.json())
-  // )
+  await wait(1.1)
+  // throw new Error('도서 목록 데이터 가져오기에 실패했습니다.')
 
-  // --------------------------------------------------------------------------
-
-  import type { QuoteResponse } from './types'
-
-  // 데이터 가져오기 비동기 함수
-  export const fetchQuotes = async (): Promise<QuoteResponse['quotes']> => {
-    // 서버 컴포넌트의 데이터 가져오기 (✅ 권장!!! 정적 렌더링 1회)
-    const response = await fetch('https://dummyjson.com/quotes')
-    const { quotes } = await response.json()
-
-    return quotes
-  }
+  return quotes
 }
