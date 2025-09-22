a30d060b4ba3b4a32bda9e150dc08baf128d3f37
HanUL072 <dbstjrals125@naver.com>
2025-09-22 15:16:09 +0900

    learn. 메타데이터 타이틀 템플릿 활용 - metadata.title.template: %s 자료 표시자 (placeholder) - metadata.title.default

--- learn-next-js/04-metadata-settings/src/app/auth/sign-up/page.tsx
+++ learn-next-js/04-metadata-settings/src/app/auth/sign-up/page.tsx
@@ -1,5 +1,16 @@
-import type { PropsWithChildren } from 'react'
+import type { Metadata } from 'next'
+import { Section } from '@/components'
 
-export default function AuthLayout({ children }: PropsWithChildren) {
-  return <div className="bg-amber-300 flex-1">{children}</div>
+export const metadata: Metadata = {
+  title: '회원가입',
+  description:
+    '회원 가입하면 OOO 이벤트에 참여할 수 있어 더 풍부한 학습이 가능해질거에요!',
+}
+
+export default function SignUpPage() {
+  return (
+    <Section title="회원가입 페이지">
+      <p>회원가입 페이지 방문</p>
+    </Section>
+  )
 }
